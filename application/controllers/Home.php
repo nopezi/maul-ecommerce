17d0849daacb7031fee64a67ade4151efcd76ac8
nopezi <snopezi@gmail.com>
2019-09-24 00:10:49 +0700

    pindah folder

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,25 +10,25 @@ class Home extends CI_Controller {
 
     public function lengan_panjang(){
         $this->load->view('content/header');
-        $this->load->view('lengan_panjang');
+        $this->load->view('frontend/lengan_panjang');
         $this->load->view('content/footer');
     }
 
     public function lengan_pendek(){
         $this->load->view('content/header');
-        $this->load->view('lengan_pendek');
+        $this->load->view('frontend/lengan_pendek');
         $this->load->view('content/footer');
     }
 
     public function hoodie(){
         $this->load->view('content/header');
-        $this->load->view('hoodie');
+        $this->load->view('frontend/hoodie');
         $this->load->view('content/footer');
     }
 
     public function detail_product(){
         $this->load->view('content/header');
-        $this->load->view('detail_product');
+        $this->load->view('frontend/detail_product');
         $this->load->view('content/footer');
     }
 
